cf3fef9daa56d3ce33ad417a845a79d0e220ea2f
srtamrakar <samyak.tamrakar@hrforecast.de>
2019-08-23 14:20:00 +0200

    removed unnecessary spaces

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
 import os
+
 from setuptools import setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding = 'utf-8') as f:
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
 	long_description = f.read()
 
 with open(os.path.join(os.path.dirname(__file__), 'requirements.txt'), encoding='utf-8') as f:
 	requirements_list = [line.rstrip('\n') for line in f]
 
 setup(
-	name = 'FreqObjectOps',
-	packages = ['FreqObjectOps'],
-	version = '0.0',
-	license = 'MIT',
-	description = 'Some special functions for some python objects.',
-	long_description = long_description,
-	long_description_content_type = 'text/markdown',
-	author = 'Samyak Ratna Tamrakar',
-	author_email = 'samyak.r.tamrakar@gmail.com',
-	url = 'https://github.com/srtamrakar/python-general-functions',
+	name='FreqObjectOps',
+	packages=['FreqObjectOps'],
+	version='0.0',
+	license='MIT',
+	description='Some special functions for some python objects.',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+	author='Samyak Ratna Tamrakar',
+	author_email='samyak.r.tamrakar@gmail.com',
+	url='https://github.com/srtamrakar/python-general-functions',
 	# download_url = 'https://github.com/srtamrakar/python-general-functions/archive/v_0.0.tar.gz',
-	keywords = ['list', 'string', 'datetime', 'directory'],
-	install_requires = requirements_list,
-	classifiers = [
+	keywords=['list', 'string', 'datetime', 'directory'],
+	install_requires=requirements_list,
+	classifiers=[
 		'Development Status :: 1 - Planning',  # Either"3 - Alpha", "4 - Beta" or "5 - Production/Stable"
 		'Intended Audience :: Developers',  # Define that your audience are developers
 		'Topic :: Software Development :: Build Tools',
